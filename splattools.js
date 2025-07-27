601c18e0ae7fd51e7e52d16f3ce0e12fdbefc718
Pierre <hirs0054@gmx.com>
2025-07-28 02:09:51 +0930

    Create splattools.js
    initial javascript with basic mesh instancing from a source mesh based on the Blockbench skeleton from the Blockbench wiki

---
+++ splattools.js
@@ -0,0 +1,58 @@
+let meshToSplatMeshButton;
+
+Plugin.register('splattools', {
+	title: 'Splat tools',
+	author: 'Pierre',
+	description: 'Tools for turning triangulated mesh sculpts into splat-texture meshes',
+	icon: 'fa-snowflake',
+	version: '0.0.1',
+	variant: 'both',
+    onload() {
+		meshToSplatMeshButton = new Action('instance_splat_tris', {
+			name: 'Create splat instance mesh',
+			description: 'Create a duplicate of the selected mesh with uniformly UV unwrapped, scaled up triangles instanced on each face of the original mesh.',
+			icon: 'fa-up-down-left-right',
+            click: function() {
+				let createdMeshes = [];
+                Undo.initEdit({
+					elements: createdMeshes,
+					selection: !0
+				});
+                const vertExpandDirection = [0, 0, 0];
+                Mesh.selected.forEach(sceneObject => {
+					let splattedMesh = new Mesh({vertices:{}});
+					splattedMesh.name = sceneObject.name + "_splat";
+					splattedMesh.rotation = sceneObject.rotation;
+					splattedMesh.origin = sceneObject.origin;
+					console.log(sceneObject);
+					console.log(splattedMesh);
+					for (const [faceID, faceObject] of Object.entries(sceneObject.faces)) {
+						const faceVertObjects = [sceneObject.vertices[faceObject.vertices[0]],sceneObject.vertices[faceObject.vertices[1]],sceneObject.vertices[faceObject.vertices[2]]];
+						const splatVertObjects = [[0,0,0],[0,0,0],[0,0,0]];
+						const splatVertexIDs = [];
+						const secondVertexToUseInScalingPair = [1,2,0];
+						for (let vertexPairNumber = 0; vertexPairNumber < 3; vertexPairNumber++) {
+							for (let vertPairScaleAxis = 0; vertPairScaleAxis < 3; vertPairScaleAxis++) {
+								splatVertObjects[vertexPairNumber][vertPairScaleAxis] = faceVertObjects[vertexPairNumber][vertPairScaleAxis] + (faceVertObjects[vertexPairNumber][vertPairScaleAxis] - faceVertObjects[secondVertexToUseInScalingPair[vertexPairNumber]][vertPairScaleAxis]) * 0.5;
+							}
+						}
+						for (let splatVertexNumber = 0; splatVertexNumber < 3; splatVertexNumber++) {
+							splatVertexIDs[splatVertexNumber] = splattedMesh.addVertices(splatVertObjects[splatVertexNumber])[0];
+						}
+						let splatFace = new MeshFace(splattedMesh,{vertices: [splatVertexIDs[0],splatVertexIDs[1],splatVertexIDs[2]]});
+						splattedMesh.addFaces(splatFace);
+					}
+					splattedMesh.init();
+					createdMeshes.push();
+					splattedMesh.select();
+				});
+                Canvas.updateAll();
+                Undo.finishEdit('Create splat instance mesh');
+            }
+        });
+        MenuBar.addAction(meshToSplatMeshButton, 'filter');
+    },
+    onunload() {
+        meshToSplatMeshButton.delete();
+    }
+});
